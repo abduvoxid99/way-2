4aec4a5c2298283b0cd407c35cc73a70068fb1d3
jamshidbek536 <jamshidbek536@gmail.com>
2025-04-09 10:58:44 +0500

    career page

--- src/pages/career/index.tsx
+++ src/pages/career/index.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 
-import Head from "next/head";
+import { ArrowRightIcon } from "lucide-react";
 import { default as Image } from "next/image";
 import Link from "next/link";
 
 import { lib } from "@/shared/lib";
 import { useLocalizedStrings } from "@/shared/lib/useLocaliizedString";
 
-import clock from "@/../public/images/clock.svg";
-import markerPin from "@/../public/images/marker-pin.svg";
-
 import Container from "@/components/common/container";
+import Typography from "@/components/common/typhography";
+import { Button } from "@/components/ui/button";
 import CSlider from "@/components/ui/slider";
 import { careers } from "@/mock/career";
 
@@ -52,24 +51,42 @@ export default function Career() {
 
   return (
     <div>
-      <Head>
-        <title>Career</title>
-      </Head>
+      <section className="py-20 bg-[#F7F8F6]">
+        <Container size="md">
+          <div className="grid grid-cols-[repeat(auto-fit,minmax(510px,1fr))] gap-5">
+            <div className="border rounded-sm py-20 px-10 ">
+              <Typography
+                as="h1"
+                variant="h1"
+                className="text-[40px] leading-[42px] ">
+                Build your career with strong company’s value and milestone
+              </Typography>
+              <Typography
+                as="h3"
+                className="text-2xl leading-[26px] font-normal mt-10">
+                Current open job vacancies available, you can find out ...
+              </Typography>
+            </div>
 
-      <section className="bg-gray-50 py-16 xl:py-24">
-        <Container>
-          <div className="text-center text-4xl font-semibold text-gray-900 xl:text-5xl">
-            Карьера
-          </div>
-          <div className="mt-4 text-center text-lg font-normal text-gray-600 xl:text-xl">
-            Познакомьтесь с нами поближе и присоединяйтесь к нам
+            <div className="w-full max-w-[510px] max-md:mx-auto rounded-sm overflow-hidden">
+              <Image
+                src="https://s3-alpha-sig.figma.com/img/53ca/3bcd/f314dcd949471f78faa8c4d38e5aadba?Expires=1744588800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=CB4T9lJFyCxnbAImaZ34AE1vJn31oyzveGMHSGRC4IEkbUGEjl3tudSjn-QRh5chJW371GvipHD1SDJsckiZLxzeLEzJg7Wts-0W3X-qcWgf-r~~rmmZKJLkA5ObNOOZDh5XzWTxWsbKJz72Zj1VTR9qW6C4UjR7nTaJgp-Xt2ErRHGBf~RBsZhZyhiM92Mp-9-0Wt2uHrVGURyixqBtSwETDlAMC2d~bpE9mogkehdF5g7PS8pPnkxLMeai1ibKgVs6xHmQgGTFU5ZLrSsyvIgE3iAMuu~Et1-3JCmHZzkPJqhlmW3h-XrcPMdzqrD2WvQOy56abAW64Z2pqtQTDA__"
+                alt="Responsive Image"
+                style={{
+                  height: "100%",
+                  objectFit: "cover",
+                }}
+                width={510}
+                height={505}
+              />
+            </div>
           </div>
         </Container>
       </section>
 
-      <section className="pt-[105px] xl:pt-32 overflow-hidden">
+      <section className="pt-[105px] xl:pt-20 overflow-hidden">
         <Container size="md">
-          <div className="border-b border-solid border-b-gray-200 pb-16 xl:pb-24">
+          <div className="pb-16 xl:pb-20">
             <div className="text-center text-3xl font-semibold text-gray-900 xl:text-4xl mb-20">
               {localizedStrings["career_title"]}
             </div>
@@ -78,15 +95,16 @@ export default function Career() {
           </div>
         </Container>
       </section>
-      {/* <section className="py-16 xl:py-24">
-        <Container>
+
+      <section className="py-16 xl:py-20 bg-[#F7F8F6]">
+        <Container size="md">
           <div className="text-center text-3xl font-semibold text-gray-900 xl:text-4xl">
             {localizedStrings["career_jobs_title"]}
           </div>
-          <div className="mt-4 text-center text-lg font-normal text-gray-600 xl:text-xl">
+          {/* <div className="mt-4 text-center text-lg font-normal text-gray-600 xl:text-xl">
             {localizedStrings["career_jobs_subtitle"]}
-          </div>
-          <div className="mt-12 justify-center lg:flex lg:flex-wrap lg:justify-normal lg:gap-x-8 xl:mt-16 xl:gap-x-16">
+          </div> */}
+          <div className="mt-20 grid grid-cols-1 md:grid-cols-2 gap-5">
             {careers?.map((item) => (
               <OpenPosition
                 key={item?.id}
@@ -97,7 +115,7 @@ export default function Career() {
             ))}
           </div>
         </Container>
-      </section> */}
+      </section>
     </div>
   );
 }
@@ -114,24 +132,15 @@ const OpenPosition = ({
   subtitle: string;
 }) => {
   const localizedStrings = useLocalizedStrings();
+
   return (
-    <div
-      key={key}
-      className="w-full border-b border-solid border-gray-200 pb-8 pt-6 first:border-t lg:w-[40%] lg:[&:nth-child(2)]:border-t">
+    <div className="w-full flex items-center justify-between p-10 border border-[#EDEDED] rounded-[5px] bg-white">
+      <div className="text-2xl font-bold text-gray-900">{title}</div>
       <Link href={`/career/${id}`} passHref>
-        <div className="text-lg font-semibold text-gray-900">{title}</div>
+        <div className="p-3 border border-[#EDEDED] rounded-[5px]">
+          <ArrowRightIcon />
+        </div>
       </Link>
-      <div className="mt-4 text-base font-normal text-gray-600">{subtitle}</div>
-      <div className="mt-6 flex gap-x-5">
-        <div className="flex items-center gap-x-2">
-          <Image src={markerPin} alt="remote" />
-          {localizedStrings["office"]}
-        </div>
-        <div className="flex items-center gap-x-2">
-          <Image src={clock} alt="full time" />
-          {localizedStrings["work_time"]}
-        </div>
-      </div>
     </div>
   );
 };
